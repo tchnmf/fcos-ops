3396a18c330510e49554756655ddc412fbfb5b9b
F.T <fthufuss@dev.null>
2022-11-07 15:24:50 +0000

    update provisioning

--- provision.sh
+++ provision.sh
@@ -8,8 +8,8 @@
 # DISK_GB=''
 # STREAM=''
 
-sudo chcon --verbose --type svirt_home_t ${IGN_CONFIG}
-sudo virt-install --connect="qemu:///system" --name="${VM_NAME}" \
+sudo -E chcon --verbose --type svirt_home_t ${IGN_CONFIG}
+sudo -E virt-install --connect="qemu:///system" --name="${VM_NAME}" \
     --vcpus="${VCPUS}" --memory="${RAM_MB}" \
     --os-variant="fedora-coreos-$STREAM" --import --graphics=none \
     --disk="size=${DISK_GB},backing_store=${IMAGE}" \
